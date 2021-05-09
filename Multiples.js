29622a8a6701ac2c82c9a6dab65a1ff394e0b38b
Ali Hamza Ahmed <81351941+ali-hamza00@users.noreply.github.com>
2021-05-10 03:40:43 +0530

    Create Multiples.js

---
+++ Multiples.js
@@ -0,0 +1,8 @@
+// find the factors of a number
+
+var a=prompt('Enter a Number to find its multiples')
+
+for(var x=1;x<=10;x++)
+{
+    document.write("<h2>"+(a)+("x")+(" ")+(x)+("=")+(" ")+(a*x)+"</h2>");
+}
