b0ab9b56afe6b72ec71d0a56c06de606eb39b1f1
Ali Hamza Ahmed <81351941+ali-hamza00@users.noreply.github.com>
2021-05-10 01:20:05 +0530

    Create positive-negative.js

---
+++ positive-negative.js
@@ -0,0 +1,12 @@
+// check if a number is positive, negative or zero
+
+var a=prompt('Enter a number');
+if (a<0) {
+    document.write("number is negative");
+} else if (a>0) {
+    document.write("number is positive");
+}
+ else {
+     document.write("number is zero");
+    
+ }
