9d232669768ce716cd484e70124d36444a07f2e6
Ali Hamza Ahmed <81351941+ali-hamza00@users.noreply.github.com>
2021-05-10 01:15:44 +0530

    Create celsius-Farenheit.js

---
+++ celsius-Farenheit.js
@@ -0,0 +1,8 @@
+// Javascript program to convert celsius to farhenheit:
+
+ var a=prompt('Degree of celsius');
+
+ //0-deg celsius = 32 farenheit
+
+ var b= a*32;
+ document.write("<h1>"+b+"</h1>");
